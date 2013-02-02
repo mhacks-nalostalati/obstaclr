cb188e479d83f15dec6cd4e844d6671643a550f5
Mayank Gulati <mynk.gulati@gmail.com>
2013-02-02 04:41:53 -0500

    adding some socket stuff

--- public/page.js
+++ public/page.js
@@ -1,13 +1,42 @@
 $(function() {
 
-  var splashmenu =  $(".splash-menu");
-  var nextbutton = $("#nextbutton");
+  var socket = io.connect(window.location.hostname);
 
-  $(document).ready(function() {
+  var fullPlayerList = [];
+
+  var splashMenu =  $(".splash-menu");
+  var playerName = $('input[name=playerName]');
+  var nextButton = $('#nextbutton');
+  var friendsName = $('input[name=friendsName]');
+
+  nextButton.click(function() {
+    splashmenu.remove();
+    var enteredName = playerName.val();
+    enteredName = enteredName.replace(/\s/g, "");
+    enteredName = enteredName.toLowerCase();
+    playerName.val(enteredName);
+    socket.emit('newPlayer', enteredName);
   });
 
-  $("#nextbutton").click(function() {
-    splashmenu.remove();
+  socket.on('fullList', function (playerList) {
+    fullPlayerList = playerList;
+    friendsName.autocomplete({
+      source: fullPlayerList;
+    });
+  });
+
+  socket.on('addPlayer', function (name) {
+    fullPlayerList.push(name);
+    friendsName.autocomplete({
+      source: fullPlayerList;
+    });
+  });
+
+  socket.on('removePlayer', function (name) {
+    fullPlayerList.splice(playerlist.indexOf(name), 1);
+    friendsName.autocomplete({
+      source: fullPlayerList;
+    });
   });
 
 });
