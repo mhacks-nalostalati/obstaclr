b974831ff98057c5eb4073a1f2cc167647024791
Mayank Gulati <mynk.gulati@gmail.com>
2013-02-02 07:46:38 -0500

    can get to game page now, lobby works perfectly

--- app.js
+++ app.js
@@ -39,10 +39,13 @@ io.configure(function () {
 });
 
 var playerlist = [];
+var clientlist = [];
+var clients = {};
 
 io.sockets.on('connection', function (socket) {
   
   //give current list on connection
+  clients[socket.id] = socket;
   io.sockets.socket(socket.id).emit('fullList', playerlist);
   console.log ("sent a list of " + playerlist.length + " players to " + socket.id);
 
@@ -52,7 +55,10 @@ io.sockets.on('connection', function (socket) {
       console.log (name + " already exists, error from " + socket.id);
       return;
     }
+
     playerlist.push(name);
+    clientlist.push(socket.id);
+
     console.log (name + " added to list from " + socket.id);
     socket.broadcast.emit('addPlayer', name);
   });
@@ -63,25 +69,47 @@ io.sockets.on('connection', function (socket) {
       console.log (name + " does not exist, error from " + socket.id);
       return;
     }
-    playerlist.splice(playerlist.indexOf(name), 1);
+
+    var position = playerlist.indexOf(name);
+    playerlist.splice(position, 1);
+    clientlist.splice(position, 1);
+
     console.log (name + " removed from list from " + socket.id);
     socket.broadcast.emit('removePlayer', name);
   });
 
+  socket.on('gameStart', function (opponent, challenger, room) {
+    var position = playerlist.indexOf(opponent);
+    var invitee = clients[clientlist[position]];
+    invitee.emit('invite', challenger, room);
+
+    socket.join(room);
+    invitee.join(room);
+
+    playerlist.splice(position, 1);
+    clientlist.splice(position, 1);
+
+    console.log (opponent + " removed from list with invite from " + socket.id);
+    invitee.broadcast.emit('removePlayer', opponent);
+  });
+
   //when a line gets created
-  socket.on('lineMade', function (x, y1, y2, color) {
+  socket.on('lineMade', function () {
     // io.sockets.emit('createLine', {});
   });
 
   //updating the player position
-
-
 
 });
 
 app.get('/', function(req, res){
   res.render('page.jade');
 });
+
+// app.get('/:room', function(req, res){
+//   //join the right room for spectating 
+//   //socket.join(req.params.room)
+// })
 
 server.listen(app.get('port'));
 console.log('server now listening on port ' + app.get('port'));
--- public/page.js
+++ public/page.js
@@ -8,45 +8,99 @@ $(function() {
 
   var splashMenu =  $(".splash-menu");
   var playerName = $('input[name=playerName]');
+  var lobbyCount = $('#lobbyCount');
   var nextButton = $('#nextbutton');
   var friendsName = $('input[name=friendsName]');
   var gameStart = $("#game-start");
+  var playButton = $('#play');
 
+  //Populate list of current players on connection
+  socket.on('fullList', function (playerList) {
+    fullPlayerList = playerList;
+    lobbyCount.text(fullPlayerList.length);
+    friendsName.autocomplete({
+      source: fullPlayerList
+    });
+  });
+
+  //Add to list everytime a player joins
+  socket.on('addPlayer', function (name) {
+    fullPlayerList.push(name);
+    lobbyCount.text(fullPlayerList.length);
+    friendsName.autocomplete({
+      source: fullPlayerList
+    });
+  });
+
+  //remove from list everytime a player leaves
+  socket.on('removePlayer', function (name) {
+    fullPlayerList.splice(fullPlayerList.indexOf(name), 1);
+    lobbyCount.text(fullPlayerList.length);
+    friendsName.autocomplete({
+      source: fullPlayerList
+    });
+  });
+
+
+  socket.on('invite', function (challenger, room) {
+    socket.removeAllListeners("addPlayer");
+    socket.removeAllListeners("removePlayer");
+
+    //possibeTodo: accept invitation
+
+    window.history.pushState(null, room, room);
+    gameStart.fadeOut('fast', function() {});
+    //todo: go to game as player
+  });
+
+  
+  socket.on('linecreated', function (x1, x2, y1, y2, color) {
+
+  });
+
+
+  //format and add the name when they come in
   nextButton.click(function() {
     name = playerName.val();
     name = name.replace(/\s/g, "");
     name = name.toLowerCase();
     playerName.val(name);
+    if (!(fullPlayerList.indexOf(name) == -1)) {
+      nextButton.prev().prev().prev().prev().text('Taken, try again');
+      return;
+    }
     socket.emit('newPlayer', name);
     splashMenu.remove();
     gameStart.fadeIn('fast', function() {});
   });
 
-  socket.on('fullList', function (playerList) {
-    fullPlayerList = playerList;
-    friendsName.autocomplete({
-      source: fullPlayerList
-    });
+  //when they click the play button
+  playButton.click(function() {
+
+    var vsName = friendsName.val().toLowerCase();
+    friendsName.val(vsName);
+    if (fullPlayerList.indexOf(vsName) == -1) return;
+
+    socket.emit('playerGone', name)
+    socket.removeAllListeners("addPlayer");
+    socket.removeAllListeners("removePlayer");
+
+    if (vsName < name) var newUrl = '/' + vsName + 'vs' + name;
+    else var newUrl = '/' + name + 'vs' + vsName;
+
+    socket.emit('gameStart', vsName, name, newUrl);
+    window.history.pushState(null, newUrl, newUrl);
+
+    gameStart.fadeOut('fast', function() {});
+    //todo: go to game as cartographer
   });
 
-  socket.on('addPlayer', function (name) {
-    fullPlayerList.push(name);
-    friendsName.autocomplete({
-      source: fullPlayerList
-    });
-  });
-
-  socket.on('removePlayer', function (name) {
-    fullPlayerList.splice(fullPlayerList.indexOf(name), 1);
-    friendsName.autocomplete({
-      source: fullPlayerList
-    });
-  });
-
-  $(window).unload(function() {
+  //remove from everyone's list if they close the window
+  $(window).on('beforeunload', function(){
     if (name) {
-      socket.emit('playerGone', name)
-    };
+      socket.emit('playerGone', name);
+      return 'please dont leave';
+    }
   });
 
 });
