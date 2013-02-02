62616802d7f567f01d6d6eedcd514023b483a876
Lesia Nalepa <lesia.nalepa@gmail.com>
2013-02-02 04:41:21 -0500

    friends name page

--- public/page.js
+++ public/page.js
@@ -2,12 +2,16 @@ $(function() {
 
   var splashmenu =  $(".splash-menu");
   var nextbutton = $("#nextbutton");
+  var gamestart = $("#game-start");
+
 
   $(document).ready(function() {
   });
 
   $("#nextbutton").click(function() {
     splashmenu.remove();
+    gamestart.fadeIn('fast', function() {
+      });
   });
 
 });
